428e119e56a056810a605f38fefe09e11ffea689
cosmineusebiupatrascu <cosmineusebiupatrascu@gmail.com>
2026-04-04 20:06:46 +0300

    added plots to prediction and requirements fix

--- teme/L2/separate_sources.py
+++ teme/L2/separate_sources.py
@@ -2,22 +2,28 @@ import torch
 import torchaudio
 from hw_utils import LibriMixDataset
 from convtasnet import ConvTasNet
+import matplotlib.pyplot as plt
 import toml
+import librosa
 
 
-model_path = 'teme/L2/models/ConvTasNet_Baseline.pth'
+
+model_path = 'teme/L2/models/ConvTasNet_Noise.pth'
+sample_key = 1
 
 val_dataset = LibriMixDataset(
     subset="val",
-    typ='both',
+    typ='sigma0',
     root='_fit/hw2/data/mini_libri2mix',
     segment_length=8,
     sample_rate=8000
 )
 
-test = val_dataset[0][0]
+test = val_dataset[sample_key][0]
 test = torch.from_numpy(test)
 test = test.unsqueeze(0)
+
+val_dataset.listen_samples(sample_key)
 
 config_path = 'teme/L2/noisy_ss.toml'
 
@@ -38,4 +44,14 @@ sample_rate = 8000
 with torch.no_grad():
     for i in range(output.shape[0]):
         channel_data = output[i:i+1, :]
+        signal = channel_data.numpy()
+
+        plt.figure(figsize=(10, 1))
+        librosa.display.waveshow(signal, sr=8000)
+        plt.title(f'Predicted_Source_{i}')
+        plt.xlabel('Time (s)')
+        plt.ylabel('Amplitude')
+        plt.ylim([-1, 1])
+        plt.show()
+
         torchaudio.save(f'source_{i}.wav', channel_data, sample_rate)
